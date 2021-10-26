e20326543226610a47891fd449c22bae17c8f402
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-10-26 21:21:50 +0600

    add

--- script.js
+++ script.js
@@ -12,5 +12,34 @@ function getpin() {
 
 function generatePin() {
     const pin = getPin();
-    document.getElementById('display-pin')
+    document.getElementById('display-pin').value = pin;
+}
+
+document.getElementById('key-pad').addEventListener('click', function(event) {
+    const number = event.target.innertext;
+    const calcInput = document.getElementById('typed-numbers');
+    if (isNaN(number)) {
+        if (number == 'C') {
+
+            calcInput.value = '';
+        }
+    } else {
+        const previousNumber = calcInput.value;
+        const newNumber = previousNumber + number;
+        calcInput.value = newNumber;
+    }
+});
+
+function verifyPin() {
+    const pin = document.createElement('display-pin').value;
+    const typedNumber = document.getElementById('typed-number').value;
+    const successMessage = document.getElementById('notify-success');
+    const failError = document.getElementById('notify-fail');
+    if (pin == typedNumber) {
+        successMessage.style.display = 'block';
+        failError.style.display = 'none';
+    } else {
+        successMessage.style.display = 'none'
+        failError.style.display = 'block'
+    }
 }
